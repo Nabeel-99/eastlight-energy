65383c87ee0b92f94eea04dcc3f8766e7f800076
Farouk Nabeel <farouknabil810@gmail.com>
2025-11-19 13:55:37 +0100

    update logo

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // disable es lint
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-} as NextConfig;
+};
 
 export default nextConfig;
